37cbd3f3c05bdfe8dc9f83c35c89f59a44928bcf
Vincent Ramos <vishyramos0026@gmail.com>
2026-09-16 02:41:04 +0800

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.photogallery',
+  appName: 'photo-gallery',
+  webDir: 'dist'
+};
+
+export default config;
---
+++ src/views/HomePage.vue
@@ -0,0 +1,29 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar> <ion-title> My Photo Gallery </ion-title> </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <CameraComponent @photo-captured="addPhoto" />
+      <PhotoGalleryComponent :photos="photos" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+import { ref } from "vue";
+import CameraComponent from "@/components/CameraComponent.vue";
+import PhotoGalleryComponent from "@/components/PhotoGalleryComponent.vue";
+
+const photos = ref<string[]>([]);
+const addPhoto = (photo: string) => {
+  photos.value.unshift(photo);
+};
+</script>
